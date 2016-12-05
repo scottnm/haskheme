cee7c5b2c8aa7302a8766b065b25d49cd6a6b89a
Scott Munro <scottnmunro@gmail.com>
2016-12-04 19:29:32 -0600

    feat: ex1.3 done

--- first_haskell_prog.hs
+++ first_haskell_prog.hs
@@ -3,5 +3,5 @@ import System.Environment
 
 main :: IO ()
 main = do
-    args <- getArgs
-    putStrLn (args !! 0 ++ " + " ++ args !! 1 ++ " = " ++ show(read(args !! 0) + read(args !! 1)))
+    name <- getLine
+    putStrLn("Hello, " ++ name)
